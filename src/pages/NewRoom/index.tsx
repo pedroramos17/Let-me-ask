a50a28cbae025a672e607b4b66e5a7edbc67c5bc
pedroramos17 <pedro_ramos2003@hotmail.com>
2022-09-13 22:06:35 -0300

    first-commit

---
+++ src/pages/NewRoom/index.tsx
@@ -0,0 +1,61 @@
+import { FormEvent, useState } from 'react'
+
+import { Link, useHistory } from 'react-router-dom';
+import { database } from '../../services/firebase';
+
+import illustrationImg from '../../assets/images/illustration.svg';
+import logoImg from '../../assets/images/logo.svg';
+
+import { Button } from '../../components/Button/index'
+import { useAuth } from '../../hooks/useAuth';
+
+import '../../styles/auth.scss';
+
+export function NewRoom() {
+  const { user } = useAuth()
+  const history = useHistory()
+  const [newRoom, setNewRoom] = useState('')
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+    if (newRoom.trim() === '') {
+      return;
+    }
+
+    const roomRef = database.ref('rooms');
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    })
+    history.push(`/rooms/${firebaseRoom.key}`)
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt='Ilustração simbolizando perguntas e respostas' />
+        <strong>Toda pergunta tem uma reposta.</strong>
+        <p>aprenda e compartilhe conhecimento com outras pessoas</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt='Letmeask' />
+          <h2>Crie uma nova sala</h2>
+          <form onSubmit={handleCreateRoom}>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+              onChange={event => setNewRoom(event.target.value)}
+              value={newRoom}
+            />
+            <Button type="submit" >
+              Criar sala
+            </Button>
+          </form>
+          <p>Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link></p>
+        </div>
+      </main>
+    </div>
+  )
+}
